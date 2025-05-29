10a61032a7c10ee7a387fe0007061df4d167ad99
Rizky <rizkydwis980@gmail.com>
2025-05-29 11:51:31 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+class MainApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Project SDA")
+        self.root.geometry("800x600")
+        self.root.resizable(False, False)
+
+        bg_image = Image.open("A.jpg")
+        bg_image = bg_image.resize((800, 600))
+        self.bg_photo = ImageTk.PhotoImage(bg_image)
+
+        bg_label = tk.Label(self.root, image=self.bg_photo)
+        bg_label.place(relwidth=1, relheight=1)
+
+        self.welcome_label = tk.Label(
+            self.root,
+            text="Selamat Datang",
+            font=("courier new", 24),
+            fg="black",
+            bg="sky blue",
+            bd=5,
+            relief="raised",
+            padx=20,
+            pady=10
+        )
+        self.welcome_label.place(relx=0.5, rely=0.15, anchor="center")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = MainApp(root)
+    root.mainloop()

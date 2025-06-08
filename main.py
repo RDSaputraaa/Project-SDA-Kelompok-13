d4508eb4ce4e66ac907f7fc6244118147d004eea
Rizky <rizkydwis980@gmail.com>
2025-06-08 15:22:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ class MainApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Project SDA")
-        self.root.geometry("800x600")
-        self.root.resizable(False, False)
+
+        self.root.attributes('-fullscreen', True)
+
+        self.screen_width = self.root.winfo_screenwidth()
+        self.screen_height = self.root.winfo_screenheight()
 
         bg_image = Image.open("A.jpg")
-        bg_image = bg_image.resize((800, 600))
+        bg_image = bg_image.resize((self.screen_width, self.screen_height))
         self.bg_photo = ImageTk.PhotoImage(bg_image)
 
         self.bg_label = tk.Label(self.root, image=self.bg_photo)
@@ -29,37 +32,37 @@ class MainApp:
         self.welcome_label = tk.Label(
             self.root,
             text="Selamat Datang\n di Project Kelompok 13",
-            font=("courier new", 24),
+            font=("courier new", 36),
             fg="black",
             bg="sky blue",
             bd=5,
             relief="raised",
-            padx=20,
-            pady=10
+            padx=50,
+            pady=20
         )
-        self.welcome_label.place(relx=0.5, rely=0.15, anchor="center")
+        self.welcome_label.place(relx=0.5, rely=0.2, anchor="center")
 
         self.enter_button = tk.Button(
             self.root,
             text="MULAI",
-            font=("courier new", 18),
+            font=("courier new", 22),
             bg="sky blue",
             relief="raised",
             fg="black",
-            padx=20,
-            pady=10
+            padx=50,
+            pady=20
         )
         self.enter_button.place(relx=0.5, rely=0.43, anchor="center")
 
         self.explain_button = tk.Button(
             self.root,
             text="PENJELASAN",
-            font=("courier new", 18),
+            font=("courier new", 22),
             bg="sky blue",
             fg="black",
             relief="raised",
-            padx=20,
-            pady=10,
+            padx=50,
+            pady=20,
             command=self.show_info
         )
         self.explain_button.place(relx=0.5, rely=0.56, anchor="center")
@@ -67,12 +70,12 @@ class MainApp:
         self.exit_button = tk.Button(
             self.root,
             text="KELUAR",
-            font=("courier new", 18),
+            font=("courier new", 22),
             bg="sky blue",
             fg="black",
             relief="raised",
-            padx=20,
-            pady=10,
+            padx=50,
+            pady=20,
             command=self.root.quit
         )
         self.exit_button.place(relx=0.5, rely=0.69, anchor="center")
@@ -88,12 +91,12 @@ class MainApp:
                  "1. Masukkan nama pemain untuk memulai permainan.\n"
                  "2. Setelah masuk ke permainan, kamu akan diberi\n   sebuah soal dengan 4 pilihan jawab.\n"
                  "3. Untuk setiap soal yang benar akan diberi\n   10 POIN, sedangkan Jika Jawaban\n   salah akan diberikan 0 POIN.\n",
-            font=("courier new", 18),
+            font=("courier new", 22),
             fg="black",
             bg="sky blue",
             bd=5,
             relief="raised",
-            padx=20,
+            padx=50,
             pady=20,
             justify="left"
         )
@@ -102,12 +105,12 @@ class MainApp:
         back_button = tk.Button(
             self.root,
             text="KEMBALI",
-            font=("courier new", 18),
+            font=("courier new", 22),
             bg="sky blue",
             fg="black",
             relief="raised",
-            padx=20,
-            pady=10,
+            padx=50,
+            pady=20,
             command=self.show_main_menu
         )
         back_button.place(relx=0.5, rely=0.75, anchor="center")
